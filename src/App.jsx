fd7f43f2791fafe9f6e18cc50826b1b6b5b43802
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-08-18 13:34:23 +0530

    add privacy message feature to counter app

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
 import Container from "./components/Container";
 import Controls from "./components/Controls";
+import PrivacyMessage from "./components/PrivacyMessage";
+import { useSelector } from "react-redux";
 
 function App() {
+  const privacy = useSelector((store) => store.privacy);
+
   return (
     <center className="px-4 py-5 my-5 text-center">
       <Container>
         <Header></Header>
         <div className="col-lg-6 mx-auto">
-          <DisplayCounter></DisplayCounter>
+          <PrivacyMessage /> : <DisplayCounter />
           <Controls></Controls>
         </div>
       </Container>
--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,17 @@ const INITIAl_VALUE = {
   privacy: false
 }
 
-const counterReducer = (store = INITIAl_VALUE, action) => {
+const counterReducer = (store = INITIAl_VALUE, action) => { 
   if (action.type === 'INCREMENT') {
-  return {counter: store.counter + 1};
+  return {...store, counter: store.counter + 1};
   } else if (action.type === 'DECREMENT') {
-    return {counter: store.counter -1};
+    return {...store, counter: store.counter -1};
   } else if (action.type === 'ADD') {
-    return {counter: store.counter + Number(action.payload.num)};
+    return {...store, counter: store.counter + Number(action.payload.num)};
   } else if (action.type === 'SUBTRACT') {
-    return {counter: store.counter - Number(action.payload.num)};
+    return {...store, counter: store.counter - Number(action.payload.num)};
   } else if (action.type === 'PRIVACY_TOGGLE') {
-    return {privacy: !store.privacy};
+    return {...store, privacy: !store.privacy};
   }
   return store;
 }
